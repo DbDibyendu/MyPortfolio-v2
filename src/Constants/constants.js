07e8746e20f82e84f00b67e77722b443ced027db
DbDibyendu <dbdibyendu5@gmail.com>
2023-06-10 22:32:05 +0530

    Completed Skills section

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -115,6 +115,14 @@ const projects = [
     name: 'Basic Calculator',
     description: 'A completely functional basic calculator website created with React. It allows you to perform mathematical calculations effortlessly. With testing implemented, the results are accurate.',
   },
+  {
+    name: "Arasartara's Gallery",
+    description: 'A memory game with an art theme implemented with React. Test your memory skills and enjoy beautiful artwork as you match pairs of cards in this engaging game.',
+  },
+  {
+    name: "Arasartara's Gallery",
+    description: 'A memory game with an art theme implemented with React. Test your memory skills and enjoy beautiful artwork as you match pairs of cards in this engaging game.',
+  },
 ];
 
 const memoji = {
@@ -134,14 +142,14 @@ const skills = [
     title: 'CSS',
     icon: cssIcon,
     description:
-    'I possess expertise in utilizing CSS to design web pages and craft visually captivating layouts that enhance the overall user experience.',
+      'I possess expertise in utilizing CSS to design web pages and craft visually captivating layouts that enhance the overall user experience.',
   },
   {
     id: 'javascript',
     title: 'JavaScript',
     icon: jsIcon,
     description:
-    'I have substantial experience in employing JavaScript to introduce interactivity and functionality into web pages, resulting in dynamic user interfaces.',
+      'I have substantial experience in employing JavaScript to introduce interactivity and functionality into web pages, resulting in dynamic user interfaces.',
   },
   {
     id: 'react',
@@ -169,14 +177,14 @@ const skills = [
     title: 'Figma',
     icon: figmaIcon,
     description:
-    'In my Figma skills, I unleash creativity, designing captivating user interfaces and collaborating seamlessly with designers and developers.',
+      'In my Figma skills, I unleash creativity, designing captivating user interfaces and collaborating seamlessly with designers and developers.',
   },
   {
     id: 'git',
     title: 'Git',
     icon: gitIcon,
     description:
-    'I am proficient in Git, managing code changes, collaborating with others, and resolving conflicts effectively.',
+      'I am proficient in Git, managing code changes, collaborating with others, and resolving conflicts effectively.',
   },
   {
     id: 'github',
