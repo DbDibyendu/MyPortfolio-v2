63a8741adf5eb62ceb2a9b097922bd19c89a00c1
DbDibyendu <dbdibyendu5@gmail.com>
2023-06-11 04:14:09 +0530

    Edited

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <div id="projects" className="w-full overflow-hidden-web flex justify-center">
       <div className="w-full min-h-[1000px] h-full xl:w-[70%] relative mt-40 flex flex-col items-center justify-center pb-36">
         <div className="w-full h-[180%] mt-[-10%] mb-[-10%]">
-          <SectionTitle title="PROJECTS" subtitle="What I have done so far" />
+          <SectionTitle title="EXPERIENCE" subtitle="What I have done so far" />
         </div>
         <div className="w-full h-[70%] z-10 flex justify-front">
           <div className="w-full xl:w-[70%] h-[-10%]">
